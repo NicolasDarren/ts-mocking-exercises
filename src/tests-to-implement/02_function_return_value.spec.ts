e64f1790d58d8293bce8d8200c71c9690446d3f7
Nicolas <n.darren.c@hotmail.com>
2021-04-30 15:15:08 +0200

    Task 2 passing (with help from Brendon)

--- src/tests-to-implement/02_function_return_value.spec.ts
+++ src/tests-to-implement/02_function_return_value.spec.ts
@@ -1,12 +1,34 @@
 import { describe, expect, it } from '@jest/globals'
 import { getAllItemsOnSale } from '../tests-to-implement/02_function_return_value'
+import * as objectGetAll from '../dependencies/get_all'
 
 describe('function mock return value', () => {
   describe('getAllItemsOnSale', () => {
-    it.skip('returns only prices under 10', async () => {
+    it('returns only prices under 10', async () => {
       // Arrange
+      const items = [{
+        id: '1',
+        name: 'item1',
+        price: 12,
+        description: 'New Item',
+        created: new Date()
+      }, {
+        id: '2',
+        name: 'item2',
+        price: 5,
+        description: 'Second Item',
+        created: new Date()
+      }];
+
+      jest.spyOn(objectGetAll, 'getAll').mockResolvedValue(items);
+
       // Act
+      const results = await getAllItemsOnSale();
+
       // Assert
+      expect(results.length).toBe(1);
+      expect(results[0].id).toBe('2');
+      expect(results[0].price).toBe(5);
     })
   })
 })
