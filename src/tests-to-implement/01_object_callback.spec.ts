5ab90b4dcc16a82125793748baaad7d75dbdb097
Nicolas <n.darren.c@hotmail.com>
2021-04-30 14:48:07 +0200

    Task 1 passing

--- src/tests-to-implement/01_object_callback.spec.ts
+++ src/tests-to-implement/01_object_callback.spec.ts
@@ -1,24 +1,55 @@
 import { describe, expect, it } from '@jest/globals'
-import { execute } from '../tests-to-implement/01_object_callback'
+import { execute, Payload } from '../tests-to-implement/01_object_callback'
 
 describe('object mock callback', () => {
   describe('execute', () => {
-    it.skip('calls the callback', () => {
+    it('calls the callback', () => {
       // Arrange
+      const stub = jest.fn();
+      const payload = {
+        id: '1',
+        amount: 10,
+        callback: stub
+      };
+
       // Act
+      execute(payload);
+
       // Assert
+      expect(stub).toBeCalled();
     })
 
-    it.skip('calls the callback once', () => {
+    it('calls the callback once', () => {
       // Arrange
+      const stub = jest.fn();
+      const payload = {
+        id: '1',
+        amount: 10,
+        callback: stub
+      };
+
       // Act
+      execute(payload);
+
       // Assert
+      expect(stub).toBeCalledTimes(1);
     })
 
-    it.skip('calls the callback with correct value', () => {
+    it('calls the callback with correct value', () => {
       // Arrange
+      const stub = jest.fn();
+      const id = '1';
+      const payload = {
+        id,
+        amount: 23,
+        callback: stub
+      };
+
       // Act
+      execute(payload);
+
       // Assert
+      expect(stub).toBeCalledWith(`230 for ${id}`);
     })
   })
 })
